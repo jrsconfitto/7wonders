121fa7e6a795abc6b60e3e8ddb77e1e22d1d9f35
jugglingnutcase <jugglingnutcase@gmail.com>
2014-03-30 19:48:17 -0400

    Add a calendar for all the games we've played

--- js/app.js
+++ js/app.js
@@ -41,12 +41,17 @@ function loaded(data, tabletop) {
     }
   }
 
+  // Show the games calendar
+  createGameCalendar(games);
+
+  // Boil down locations
   locations = d3.nest()
     .key(function(player) {
       return player.location
     })
     .entries(people);
 
+  // Show the player blocks
   players = d3.nest()
     .key(function(player) { return player.name })
     .sortKeys(d3.ascending)
@@ -141,4 +146,92 @@ function showUser(data) {
   console.log(data);
 
   var selectedPlayer = d3.select('#selectedPlayer');
+}
+
+// Game calendar. Cribbed from http://bl.ocks.org/mbostock/4063318
+function createGameCalendar(games) {
+  var width = 960,
+      height = 136,
+      cellSize = 17; // cell size
+
+  var day = d3.time.format("%w"),
+      week = d3.time.format("%U"),
+      format = d3.time.format("%Y-%m-%d");
+
+  var color = d3.scale.quantize()
+      .domain([-.05, .05])
+      .range(d3.range(11).map(function(d) { return "q" + d + "-11"; }));
+
+  // Get date range of games
+  var byYear = function(game) {
+    return game.date.getUTCFullYear();
+  }
+
+  var yearRange = d3.range(d3.min(games, byYear),
+                           d3.max(games, byYear) + 1);
+
+  // Make a calendar for each year in our game date range
+  var svg = d3.select("#calendar").selectAll("svg")
+      .data(yearRange)
+    .enter().append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("class", "RdYlGn")
+    .append("g")
+      .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
+
+  svg.append("text")
+      .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d; });
+
+  var rect = svg.selectAll(".day")
+      .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+    .enter().append("rect")
+      .attr("class", "day")
+      .attr("width", cellSize)
+      .attr("height", cellSize)
+      .attr("x", function(d) { return week(d) * cellSize; })
+      .attr("y", function(d) { return day(d) * cellSize; })
+      .datum(format);
+
+  rect.append("title")
+      .text(function(d) { return d; });
+
+  svg.selectAll(".month")
+      .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+    .enter().append("path")
+      .attr("class", "month")
+      .attr("d", monthPath);
+
+  // Fill in the calendar day rects
+  var gameDates = games.map(function(game) {
+    return format(game.date)
+  });
+
+  rect.filter(function(d) {
+        return gameDates.indexOf(d) !== -1
+      })
+      .attr("class", function(d) {
+        var gameCount = games.filter(function(game) { return format(game.date) == d })
+        return "day " + color(gameCount.length);
+      })
+    .select("title")
+      .text(function(d) {
+        var gameCount = games.filter(function(game) { return format(game.date) == d })
+        return d + ": " + gameCount.length + ' ' + ((gameCount.length > 1) ? 'games': 'game');
+      });
+
+  function monthPath(t0) {
+    var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+        d0 = +day(t0), w0 = +week(t0),
+        d1 = +day(t1), w1 = +week(t1);
+    return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
+        + "H" + w0 * cellSize + "V" + 7 * cellSize
+        + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
+        + "H" + (w1 + 1) * cellSize + "V" + 0
+        + "H" + (w0 + 1) * cellSize + "Z";
+  }
+
+  // d3.select(self.frameElement).style("height", "2910px");
 }
